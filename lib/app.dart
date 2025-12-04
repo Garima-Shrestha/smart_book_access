7775cbd8cc3fc8523568d664e2eb1d36e24589cd
garima shrestha <garimashrestha20@gmail.com>
2025-12-04 13:46:59 +0545

    Images folder with few images added, few changes in pubspec.yaml for images

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:smart_book_access/screens/splash_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+
+      debugShowCheckedModeBanner: false,    // debug banner hatauna
+    );
+  }
+}
