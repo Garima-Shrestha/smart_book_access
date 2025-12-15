d577f59e5b1afdd72caa583d7d9653203db19ca9
garima shrestha <garimashrestha20@gmail.com>
2025-12-15 10:21:02 +0545

    Model from homescreen created

---
+++ lib/model/book.dart
@@ -0,0 +1,11 @@
+class Book {
+  final String bookName;
+  final String authName;
+  final String image;
+
+  Book ({
+    required this.bookName,
+    required this.authName,
+    required this.image,
+  });
+}
