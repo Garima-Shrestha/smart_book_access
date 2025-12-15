f10420dc8c96c921b740725e8e2725dc85c73c88
garima shrestha <garimashrestha20@gmail.com>
2025-12-15 14:31:07 +0545

    Theme added in app file

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_book_access/screens/splash_screen.dart';
+import 'package:smart_book_access/theme/theme_data.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,6 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: getApplicationTheme(),
       home: SplashScreen(),
 
       debugShowCheckedModeBanner: false,    // debug banner hatauna
