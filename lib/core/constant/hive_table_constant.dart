d9150b20ba76dd39acf24d1e4a521eec36ce9304
garima shrestha <garimashrestha20@gmail.com>
2025-12-31 17:03:52 +0545

    Hive Table Constant Done

---
+++ lib/core/constant/hive_table_constant.dart
@@ -0,0 +1,19 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  // Database name
+  static const String dbName= 'novella_db';
+
+  // Table name
+  // User
+  static const int userTypeId = 0;
+  static const String userTable = 'user_table';
+
+  // Book
+  static const int bookTypeId = 1;
+  static const String bookTable = 'book_table';
+
+  // Rental History
+  static const int rentalTypeId = 2;
+  static const String rentalTable = 'rental_table';
+}
