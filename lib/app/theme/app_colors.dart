f8d293f513d26325507e493ce3fe6433d617694d
garima shrestha <garimashrestha20@gmail.com>
2026-01-01 10:50:54 +0545

    Color for status only done

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+AppColors._();
+
+  // Status Colors
+  static const Color success = Color(0xFF4CAF50); // green
+  static const Color warning = Color(0xFFFFA726); // mix of yellow and orange
+  static const Color error = Color(0xFFEF4444); // red
+  static const Color info = Color(0xFF3B82F6); // blue
+}
