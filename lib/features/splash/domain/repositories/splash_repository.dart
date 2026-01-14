159b2ef692b97bab235220d330908c0d094b971b
garima shrestha <garimashrestha20@gmail.com>
2026-01-14 17:04:17 +0545

    Unnecessary files for splash page deleted

--- lib/features/splash/domain/repositories/splash_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/error/failures.dart';
-
-abstract interface class ISplashRepository {
-  // Returns true if the user is already logged in
-  Future<Either<Failure, bool>> isUserLoggedIn();
-}
