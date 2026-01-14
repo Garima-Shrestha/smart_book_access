ee16bf97f445e9d0e052373c28c442b98f60ff09
garima shrestha <garimashrestha20@gmail.com>
2026-01-14 17:18:01 +0545

    Changes made in the splash page for Shared Preference

--- lib/features/splash/presentation/page/splash_page.dart
+++ lib/features/splash/presentation/page/splash_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lost_n_found/app/routes/app_routes.dart';
+import 'package:lost_n_found/core/services/storage/user_session_service.dart';
 import 'package:lost_n_found/features/dashboard/presentation/page/dashboard_page.dart';
 import 'package:lost_n_found/features/onboarding/presentation/page/onboarding_page.dart';
-import 'package:lost_n_found/features/splash/presentation/view_model/splash_view_model.dart';
 
 class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
@@ -17,18 +17,24 @@ class _SplashPageState extends ConsumerState<SplashPage> {
   @override
   void initState() {
     super.initState();
-
-    Future.delayed(Duration(seconds: 2), () async {
-      await ref.read(splashViewModelProvider.notifier).checkInitialPage();
-      final state = ref.read(splashViewModelProvider);
-
-      if (state.isLoggedIn == true) {
-        AppRoutes.pushReplacement(context, const DashboardPage());
-      } else {
-        AppRoutes.pushReplacement(context, const OnboardingPage());
-      }
-    });
+    _navigateToNext();
   }
+
+  Future<void> _navigateToNext() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (!mounted) return;
+
+    // Check if user is already logged in
+    final userSessionService = ref.read(userSessionServiceProvider);
+    final isLoggedIn = userSessionService.isLoggedIn();
+
+    if (isLoggedIn) {
+      AppRoutes.pushReplacement(context, const DashboardPage());
+    } else {
+      AppRoutes.pushReplacement(context, const OnboardingPage());
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
