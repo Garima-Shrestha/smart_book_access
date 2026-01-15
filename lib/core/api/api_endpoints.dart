1083c732d131a21f08ca4c096454798348e2a9f6
garima shrestha <garimashrestha20@gmail.com>
2026-01-15 17:29:10 +0545

    Url corrected

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -2,14 +2,15 @@ class ApiEndpoints {
   ApiEndpoints._();
 
   // Base URL
-  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';
+  // static const String baseUrl = 'http://10.0.2.2:3000/api/v1';
+  static const String baseUrl = 'http://10.0.2.2:5050';
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 
   // AUTH
-  static const String register = '/auth/register';
-  static const String login = '/auth/login';
-  static const String logout = '/auth/logout';
-  static String authById(String id) => '/auth/$id';
+  static const String register = '/api/auth/register';
+  static const String login = '/api/auth/login';
+  static const String logout = '/api/auth/logout';
+  static String authById(String id) => '/api/auth/$id';
 }
