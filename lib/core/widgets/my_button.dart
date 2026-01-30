4a663ffef64ff9d8e19d681edaba8320b6948066
garima shrestha <garimashrestha20@gmail.com>
2026-01-30 09:46:38 +0545

    Button widget no change

--- lib/core/widgets/my_button.dart
+++ lib/core/widgets/my_button.dart
@@ -17,37 +17,34 @@ class MyButton extends StatelessWidget {
   final double fontSize;
   final bool isLoading;
 
-
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: color ?? Color(0xFF00354B),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color ?? Color(0xFF00354B),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      ),
+      onPressed: onPressed,
+      child: isLoading
+          ? const SizedBox(
+              width: 24,
+              height: 24,
+              child: CircularProgressIndicator(
+                strokeWidth: 2.5,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
             )
-        ),
-        onPressed: onPressed,
-        child: isLoading
-            ? const SizedBox(
-            width: 24,
-            height: 24,
-            child: CircularProgressIndicator(
-              strokeWidth: 2.5,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          : Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize:
+                    fontSize, // here by keeping this we can change the fontsize if needed, if we want to keep 24 instead of 18 then we can do it for that particular page
+              ),
             ),
-        )
-            : Text(
-          text,
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: fontSize,  // here by keeping this we can change the fontsize if needed, if we want to keep 24 instead of 18 then we can do it for that particular page
-          ),
-        ),
     );
   }
 }
